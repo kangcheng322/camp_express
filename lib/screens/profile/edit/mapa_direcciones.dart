c16f2a0483909e5c1aba2464dcade32aab717b8a
damn21 <dmezae@uninorte.edu.co>
2022-05-10 09:29:10 -0500

    feat:  🔨 screen file updated mapa_direcciones.dart

--- lib/screens/profile/edit/mapa_direcciones.dart
+++ lib/screens/profile/edit/mapa_direcciones.dart
@@ -1,72 +1,98 @@
 import 'dart:async';
+import 'dart:collection';
 
-import 'package:camp_express/controller/address_controller.dart';
-import 'package:camp_express/domain/direccion.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../../controller/address_controller.dart';
+import '../../../domain/direccion.dart';
+
 class MapaDirecciones extends StatefulWidget {
   @override
-  State<MapaDirecciones> createState() => MapaDireccionesState();
+  State<MapaDirecciones> createState() => MapSampleState();
 }
 
-class MapaDireccionesState extends State<MapaDirecciones> {
-  @override
+class MapSampleState extends State<MapaDirecciones> {
   void initState() {
     super.initState();
-    //getMarkers();
+    getMarkers();
   }
-
-  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
-
-  // getMarkers() async {
-  //   AddressController addressController = Get.find();
-  //   await addressController.getAddresses();
-  //   int cont = 0;
-  //   for (Direccion i in addressController.addressesList) {
-  //     Marker marker = Marker(
-  //         markerId: MarkerId(cont.toString()),
-  //         position: LatLng(double.parse(i.latitud), double.parse(i.longitud)),
-  //         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
-  //         infoWindow: InfoWindow(
-  //             title: "Hytech City",
-  //             onTap: () {},
-  //             snippet: "Snipet Hitech City"));
-  //     // markers[MarkerId(cont.toString())] = marker;
-  //     cont++;
-  //   }
-  // }
 
   Completer<GoogleMapController> _controller = Completer();
 
-  // ignore: prefer_const_declarations
-  static final CameraPosition _kGooglePlex = const CameraPosition(
+  // Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  final Set<Marker> markers = new Set();
+  var myMarkers = HashSet<Marker>();
+
+  testDireccion() {
+    markers.add(Marker(
+      //add first marker
+      markerId: MarkerId('1'),
+      position: LatLng(
+          double.parse('30.7'), double.parse('27.8')), //position of marker
+      infoWindow: InfoWindow(
+        //popup info
+        title: 'My Custom Title ',
+        snippet: 'My Custom Subtitle',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+  }
+
+  getMarkers() {
+    AddressController addressController = Get.find();
+    int cont = 0;
+    for (Direccion i in addressController.addressesList) {
+      printInfo(info: 'INFO => ' + i.latitud);
+      Marker marker = Marker(
+          markerId: MarkerId(cont.toString()),
+          position: LatLng(double.parse(i.latitud), double.parse(i.longitud)),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+          infoWindow: InfoWindow(
+              title: "Hytech City",
+              onTap: () {},
+              snippet: "Snipet Hitech City"));
+      // markers[MarkerId(cont.toString())] = marker;
+      cont++;
+    }
+    print('CONT => ' + cont.toString());
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  // ignore: prefer_const_constructors
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: const LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       body: GoogleMap(
-          //mapType: MapType.hybrid,
-          initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-          markers: Set<Marker>.of(markers.values)),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        // mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          setState(() {
+            AddressController addressController = Get.find();
+            int cont = 0;
+            for (Direccion i in addressController.addressesList) {
+              myMarkers.add(
+                Marker(
+                    markerId: MarkerId(cont.toString()),
+                    position: LatLng(
+                        double.parse(i.latitud), double.parse(i.longitud))),
+              );
+              cont++;
+            }
+          });
+        },
+        markers: myMarkers,
       ),
     );
   }
